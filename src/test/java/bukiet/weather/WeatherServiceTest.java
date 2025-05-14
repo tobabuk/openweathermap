7dbbc275dbef9c572d87c621789c626cbf0dc574
Toba Bukiet <tobabukiet@gmail.com>
2025-05-14 17:23:14 -0400

    fixes

--- src/test/java/bukiet/weather/WeatherServiceTest.java
+++ src/test/java/bukiet/weather/WeatherServiceTest.java
@@ -13,7 +13,7 @@ class WeatherServiceTest {
         WeatherService service = new WeatherServiceFactory().getService();
     ApiKey apiKey = new ApiKey();
      String keyString = apiKey.get();
-        WeatherResponse response = service.weatherNow("Edison",keyString,"imperial").blockingGet();
+        WeatherResponse response = service.weatherNow("Edison", keyString, "imperial").blockingGet();
         assertTrue(response.main.temp > 0);
 
     }
